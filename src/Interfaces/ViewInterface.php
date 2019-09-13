efc7fcccd88d61e30191bc88bf2ac600af609448
ueef <artem@arkadev.org>
2019-09-13 15:22:37 +0500

    fix ViewInterface

--- src/Interfaces/ViewInterface.php
+++ src/Interfaces/ViewInterface.php
@@ -4,6 +4,6 @@ namespace Ueef\Owlet\Interfaces {
 
     interface ViewInterface
     {
-        public function render($views, array $args, string $content = null): ?string;
+        public function render($views, array $args = [], string $content = null): ?string;
     }
 }
